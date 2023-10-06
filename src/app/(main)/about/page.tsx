c8fa2b7fc0b0edc6a9ce9f26e404320a08756cc9
Farshid <farenjir@gmail.com>
2023-10-06 23:32:00 +0330

    langs

---
+++ src/app/(main)/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+	return (
+		<center>
+			<h1>AboutPage</h1>
+		</center>
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="fa">
+		<html lang="fa" dir="rtl">
 			<body>
 				<GlobalStates>{children}</GlobalStates>
 			</body>
