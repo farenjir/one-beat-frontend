21ebbef987e332ba1fd7b3ccdc87b85e4c48453e
Farshid <farenjir@gmail.com>
2023-10-06 18:49:27 +0330

    init_langs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  experimental: {
-    // appDir: true,
-  },
-}
-
+	i18n: {
+		locales: ["fa", "en"],
+		defaultLocale: "fa",
+		domains: [
+			// {
+			// 	domain: "example.ir",
+			// 	defaultLocale: "fa",
+			// },
+			// {
+			// 	domain: "example.com",
+			// 	defaultLocale: "en",
+			// },
+			{
+				domain: "localhost",
+				defaultLocale: "fa",
+				http: true,
+			},
+		],
+	},
+};
