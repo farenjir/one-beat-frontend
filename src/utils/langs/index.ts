6f185e3e2b65dd401ecf3bea1d55fc40c790583a
Farshid <farenjir@gmail.com>
2023-10-10 00:42:19 +0330

    int_lng_types

--- src/utils/langs/index.ts
+++ src/utils/langs/index.ts
@@ -4,11 +4,11 @@ export type ILocale = "fa" | "en";
 
 export const locales = ["fa", "en"];
 
-export const defaultLocale = "fa";
+export const lDefault = "fa";
 
 const dictionaries = {
 	fa: () => import("./fa.json").then((module) => module.default),
 	en: () => import("./en.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: ILocale) => await dictionaries[locale || defaultLocale]();
+export const getDictionary = async (locale: ILocale) => await dictionaries[locale || lDefault]();
