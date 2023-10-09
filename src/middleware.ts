f9a30cc04f99c128c312ee9ea5c9505d9526a1b5
Farshid <farenjir@gmail.com>
2023-10-10 00:40:23 +0330

    int_es_lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 import { locales } from "./utils/langs";
 
 // Get the preferred locale, similar to the above or using a library
-function getLocale(request: any) {
+function getLocale(request: NextRequest) {
 	return "fa";
 }
 
-export function middleware(request: any) {
+export function middleware(request: NextRequest) {
 	// Check if there is any supported locale in the pathname
 	const pathname = request.nextUrl.pathname;
 	const pathnameHasLocale = locales.some((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`);
