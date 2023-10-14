d9994bcc31fbd57651b482e0e914b4e329fe15a8
Farshid Enjili <f.enjili@fwutech.com>
2023-10-14 18:59:35 +0330

    bg_metro_no_layout

--- src/app/[locale]/(main)/_library/components/TopProduct/index.tsx
+++ src/app/[locale]/(main)/_library/components/TopProduct/index.tsx
@@ -14,9 +14,38 @@ export default function TopProduct({ dict }: PropsWithDice) {
 		<section className="pb-10 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 			<Carousel autoplay dots waitForAnimate dotPosition="bottom" easing="slow">
 				<div className="h-[950px] leading-[950px] lg:h-[450px] lg:leading-[450px] text-white">
-					<section className="flex flex-col md:flex-row-reverse">
+					<section className="flex flex-col md:flex-row">
 						<div className="grow-0">
-							<Image className="w-full rounded" src={img} alt="" loading="lazy" />
+							<Image className="w-full rounded-xl" src={img} alt="" loading="lazy" />
+						</div>
+						<div className="grow-0">
+							<div className="flex flex-col justify-between p-4 leading-normal">
+								<h5 className="mb-2 text-2xl font-bold tracking-tight ">
+									Noteworthy technology acquisitions 2021
+								</h5>
+								<p className="mb-3 font-normal">
+									Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+									chronological order.
+								</p>
+							</div>
+						</div>
+						<div className="grow-0">
+							<div className="flex flex-col justify-between p-4 leading-normal">
+								<h5 className="mb-2 text-2xl font-bold tracking-tight  ">
+									Noteworthy technology acquisitions 2021
+								</h5>
+								<p className="mb-3 font-normal">
+									Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+									chronological order.
+								</p>
+							</div>
+						</div>
+					</section>
+				</div>
+				<div className="h-[950px] leading-[950px] lg:h-[450px] lg:leading-[450px] text-white">
+					<section className="flex flex-col md:flex-row">
+						<div className="grow-0">
+							<Image className="w-full rounded-xl" src={img} alt="" loading="lazy" />
 						</div>
 						<div className="grow-0">
 							<div className="flex flex-col justify-between p-4 leading-normal">
