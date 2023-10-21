69934b2324ff0164b54cf354ac22116a04ed4ff6
Farshid <farenjir@gmail.com>
2023-10-22 01:18:47 +0330

    init_pwa_and_manifests

--- src/meta/mainLayout.ts
+++ src/meta/mainLayout.ts
@@ -4,11 +4,14 @@ import { GenerateMetaProps } from "@/types/configs";
 import { getDictionary } from "@/langs";
 
 export async function mainLayoutMetadata(parameters: GenerateMetaProps, parent: ResolvingMetadata): Promise<Metadata> {
-	const { params, searchParams } = parameters;
+	const {
+		searchParams,
+		params: { locale },
+	} = parameters;
 	// getDictionary
 	const {
 		App: { DEFAULT_TITLE, DESCRIPTION, NAME, TITLE_TEMPLATE },
-	} = await getDictionary(params.locale);
+	} = await getDictionary(locale);
 	// return
 	return {
 		applicationName: NAME,
@@ -24,26 +27,26 @@ export async function mainLayoutMetadata(parameters: GenerateMetaProps, parent: 
 			title: DEFAULT_TITLE,
 			// startUpImage: [],
 		},
-		manifest: `/assets/${params.locale}_manifest.json`,
+		manifest: `/assets/${locale}_manifest.json`,
 		formatDetection: {
 			telephone: false,
 		},
-		// openGraph: {
-		// 	type: "website",
-		// 	siteName: APP_NAME,
-		// 	title: {
-		// 		default: APP_DEFAULT_TITLE,
-		// 		template: APP_TITLE_TEMPLATE,
-		// 	},
-		// 	description: APP_DESCRIPTION,
-		// },
-		// twitter: {
-		// 	card: "summary",
-		// 	title: {
-		// 		default: APP_DEFAULT_TITLE,
-		// 		template: APP_TITLE_TEMPLATE,
-		// 	},
-		// 	description: APP_DESCRIPTION,
-		// },
+		openGraph: {
+			type: "website",
+			siteName: NAME,
+			title: {
+				default: DEFAULT_TITLE,
+				template: TITLE_TEMPLATE,
+			},
+			description: DESCRIPTION,
+		},
+		twitter: {
+			card: "summary",
+			title: {
+				default: DEFAULT_TITLE,
+				template: TITLE_TEMPLATE,
+			},
+			description: DESCRIPTION,
+		},
 	};
 }
