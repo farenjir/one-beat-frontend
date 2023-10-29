596890e7436bbd3fd122f0850e366faaaf828a48
Farshid <farenjir@gmail.com>
2023-10-29 22:16:18 +0330

    store_refactoring

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,12 @@
-import { GenerateMetaProps, PropsWithParams, PropsWithDice, IParams } from "./configs";
-
-import { IUser } from "./public/user";
+import { Roles } from "./enums";
+import type { GenerateMetaProps, PropsWithParams, PropsWithDice, IParams } from "./configs";
+import type { IUser } from "./public/user";
 
 // configs
 export type { GenerateMetaProps, PropsWithParams, PropsWithDice, IParams };
 
 // data types
 export type { IUser };
+
+// app enums
+export { Roles };
