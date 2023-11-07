f485fbece7dd73ab9fa12f8fad047ed986058ddd
Farshid Enjili <f.enjili@fwutech.com>
2023-11-07 18:22:02 +0330

    next_configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 /** @type {import("next").NextConfig} */
 
 const { NEXT_APP_BASE_URL, NEXT_APP_BACKEND_SERVER, NODE_ENV } = process.env;
@@ -8,21 +6,30 @@ const { PHASE_DEVELOPMENT_SERVER: development, PHASE_PRODUCTION_BUILD: productio
 const nextConfig = {
 	reactStrictMode: true,
 	env: { NEXT_APP_BASE_URL, NEXT_APP_BACKEND_SERVER },
+	// eslint: {
+	//   ignoreDuringBuilds: true,
+	// },
+	// typescript: {
+	// 	ignoreBuildErrors: true,
+	// },
+	// generateBuildId: async () => "my-build-id",
+	// generateEtags: false,
+	// webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => config,
 };
 
 module.exports = (/** @type {string} */ phase, { defaultConfig }) => {
-	// if (phase === development || phase === production) {
-	// 	const withPWA = require("@ducanh2912/next-pwa").default({
-	// 		dest: "public/pwa",
-	// 		disable: false, // NODE_ENV !== "production"
-	// 		register: true,
-	// 		scope: "/",
-	// 		// sw: "service-worker.js",
-	// 		// customWorkerSrc: "service-worker",
-	// 		// customWorkerDest: "somewhere-else", // defaults to `dest`
-	// 		// customWorkerPrefix: "not/a-worker",
-	// 	});
-	// 	return withPWA(nextConfig);
-	// }
+	if (phase === development || phase === production) {
+		const withPWA = require("@ducanh2912/next-pwa").default({
+			dest: "public/pwa",
+			disable: false, // NODE_ENV !== "production"
+			register: true,
+			scope: "/",
+			// sw: "service-worker.js",
+			// customWorkerSrc: "service-worker",
+			// customWorkerDest: "somewhere-else", // defaults to `dest`
+			// customWorkerPrefix: "not/a-worker",
+		});
+		return withPWA(nextConfig);
+	}
 	return nextConfig;
 };
