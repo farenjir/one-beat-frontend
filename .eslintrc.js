ce58906c5779ffa615429d652b5eaab2bca6afa3
Farshid Enjili <f.enjili@fwutech.com>
2023-11-14 17:30:47 +0330

    install_husky_on_app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
 	root: true,
 	extends: ["next/core-web-vitals"],
-	rules: {},
+	rules: {
+		"react/display-name": "off",
+	},
 	plugins: ["testing-library"],
 	overrides: [
 		{
