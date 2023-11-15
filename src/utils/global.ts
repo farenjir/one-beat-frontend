77ca7e5168f3496244a3b63550505f8333c066b6
Farshid Enjili <f.enjili@fwutech.com>
2023-11-15 12:34:29 +0330

    refactoring_templates

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,5 +1,7 @@
 import { ILocale, ILocaleOptions } from "@/types";
+
 import { getFromCookie } from "./cookie";
+import { defaultLocale } from "@/langs";
 
 export const localeOptions: ILocaleOptions = {
 	fa: { lang: "fa", dir: "rtl", rtl: true, ltr: false, fontType: "yekan" },
@@ -7,6 +9,6 @@ export const localeOptions: ILocaleOptions = {
 };
 
 export const getLocaleConfigs = (locale?: ILocale | undefined) => {
-	const currentLang: ILocale = locale ?? getFromCookie("locale") ?? "fa";
+	const currentLang: ILocale = locale ?? getFromCookie("locale") ?? defaultLocale;
 	return localeOptions[currentLang];
 };
