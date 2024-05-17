51af1ce6c141aff365cd33fc8e55a3401629bac7
Farshid <farenjir@gmail.com>
2024-05-17 18:32:19 +0330

    music_player_edit

--- src/app/components/MusicPlayer/TailwindPlayer.tsx
+++ src/app/components/MusicPlayer/TailwindPlayer.tsx
@@ -6,8 +6,8 @@ export default function MusicPlayer() {
 			<div className="bg-white border-slate-100 dark:bg-slate-800 dark:border-slate-500 border-b rounded-t-xl p-4 pb-6 sm:p-10 sm:pb-8 lg:p-6 xl:p-10 xl:pb-8 space-y-6 sm:space-y-8 lg:space-y-6 xl:space-y-8">
 				<div className="flex items-center space-x-4">
 					<Image
-						src="/full-stack-radio.png"
-						alt=""
+						src="/assets/images/test.jpg"
+						alt="test-banner"
 						width="88"
 						height="88"
 						className="flex-none rounded-lg bg-slate-100"
@@ -26,14 +26,14 @@ export default function MusicPlayer() {
 				<div className="space-y-2">
 					<div className="relative">
 						<div className="bg-slate-100 dark:bg-slate-700 rounded-full overflow-hidden">
-							{/* <div
+							<div
 								className="bg-cyan-500 dark:bg-cyan-400 w-1/2 h-2"
 								role="progressbar"
 								aria-label="music progress"
-								aria-valuenow="1456"
-								aria-valuemin="0"
-								aria-valuemax="4550"
-							></div> */}
+								aria-valuenow={1456}
+								aria-valuemin={0}
+								aria-valuemax={4550}
+							/>
 						</div>
 						<div className="ring-cyan-500 dark:ring-cyan-400 ring-2 absolute left-1/2 top-1/2 w-4 h-4 -mt-2 -ml-2 flex items-center justify-center bg-white rounded-full shadow">
 							<div className="w-1.5 h-1.5 bg-cyan-500 dark:bg-cyan-400 rounded-full ring-1 ring-inset ring-slate-900/5"></div>
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,6 @@
 // global spatial app components for this project
 
-export { default as MusicPlayer } from "./MusicPlayer";
+export { default as MusicPlayer } from "./MusicPlayer/TailwindPlayer";
 
 export { default as ProductCard } from "./Cards/ProductCard";
 export { default as ProducerCard } from "./Cards/ProducerCard";
